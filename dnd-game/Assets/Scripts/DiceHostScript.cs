49ffe11ead612a9647737f38fa8a8fd4e7886571
guybenda <guy@benda.dev>
2023-12-28 23:09:17 +0200

    Common lib, structure project

---
+++ dnd-game/Assets/Scripts/DiceHostScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using DndCommon;
+using UnityEngine;
+
+public class DiceHostScript : MonoBehaviour
+{
+    public Dice test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Awake()
+    {
+
+    }
+}
---
+++ dnd-game/Assets/Scripts/common/Dice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DndCommon
+{
+    [Serializable]
+    public class Dice
+    {
+        public int Sides { get; set; }
+        public int Count { get; set; }
+        public int Modifier { get; set; }
+
+        public Dice(int sides, int count, int modifier)
+        {
+            Sides = sides;
+            Count = count;
+            Modifier = modifier;
+        }
+
+        public Dice(int sides, int count) : this(sides, count, 0)
+        {
+        }
+
+        public Dice(int sides) : this(sides, 1, 0)
+        {
+        }
+
+        public Dice() : this(20, 1, 0)
+        {
+        }
+
+        public int Roll()
+        {
+            var random = new Random();
+            var total = 0;
+            for (var i = 0; i < Count; i++)
+            {
+                total += random.Next(1, Sides + 1);
+            }
+
+            return total + Modifier;
+        }
+
+        public override string ToString()
+        {
+            return $"{Count}d{Sides}{(Modifier > 0 ? "+" : "")}{(Modifier != 0 ? Modifier.ToString() : "")}";
+        }
+    }
+}
